391fb0c66a9d2905409573ae25f54edaec1bffd7
Yagul <117384757+Yagul@users.noreply.github.com>
2024-06-14 14:55:12 +0530

    Add files via upload

---
+++ TreeView.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SDD_Menu
+{
+    partial class TreeViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddNodeButton = new System.Windows.Forms.Button();
+            this.RemoveNodeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(92, 143);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(269, 361);
+            this.treeView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(281, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tree View !!!";
+            // 
+            // AddNodeButton
+            // 
+            this.AddNodeButton.Location = new System.Drawing.Point(497, 179);
+            this.AddNodeButton.Name = "AddNodeButton";
+            this.AddNodeButton.Size = new System.Drawing.Size(132, 37);
+            this.AddNodeButton.TabIndex = 2;
+            this.AddNodeButton.Text = "Add Node";
+            this.AddNodeButton.UseVisualStyleBackColor = true;
+            // 
+            // RemoveNodeButton
+            // 
+            this.RemoveNodeButton.Location = new System.Drawing.Point(497, 289);
+            this.RemoveNodeButton.Name = "RemoveNodeButton";
+            this.RemoveNodeButton.Size = new System.Drawing.Size(132, 41);
+            this.RemoveNodeButton.TabIndex = 3;
+            this.RemoveNodeButton.Text = "Remove Node";
+            this.RemoveNodeButton.UseVisualStyleBackColor = true;
+            // 
+            // TreeViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 592);
+            this.Controls.Add(this.RemoveNodeButton);
+            this.Controls.Add(this.AddNodeButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "TreeViewForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddNodeButton;
+        private System.Windows.Forms.Button RemoveNodeButton;
+    }
+}
---
+++ TreeView.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDD_Menu
+{
+    public partial class TreeViewForm : Form
+    {
+        public TreeViewForm()
+        {
+            InitializeComponent();
+
+            treeView1.Nodes.Add("Sports");
+            treeView1.Nodes[0].Nodes.Add("Indoor");
+            treeView1.Nodes[0].Nodes[0].Nodes.Add("Carrom");
+            treeView1.Nodes[0].Nodes[0].Nodes.Add("Cards");
+            treeView1.Nodes[0].Nodes.Add("Outdoor");
+            treeView1.Nodes[0].Nodes[1].Nodes.Add("Cricket");
+            treeView1.Nodes[0].Nodes[1].Nodes.Add("Football");
+            treeView1.Nodes.Add("Food");
+            treeView1.Nodes[1].Nodes.Add("Breakfast");
+            treeView1.Nodes[1].Nodes[0].Nodes.Add("Dosa");
+            treeView1.Nodes[1].Nodes[0].Nodes.Add("Idly");
+            treeView1.Nodes[1].Nodes.Add("Lunch");
+            treeView1.Nodes[1].Nodes[1].Nodes.Add("Cricket");
+            treeView1.Nodes[1].Nodes[1].Nodes.Add("Football");
+            treeView1.Nodes[1].Nodes.Add("Dinner");
+            treeView1.Nodes[1].Nodes[2].Nodes.Add("Cricket");
+            treeView1.Nodes[1].Nodes[2].Nodes.Add("Football");
+           
+
+            treeView1.CheckBoxes = true;
+            AddNodeButton.Click += AddNodeButton_Click;
+            RemoveNodeButton.Click += RemoveNodeButton_Click;
+        }
+
+        private void AddNodeButton_Click(object sender, EventArgs e)
+        {
+            TreeNode newNode = new TreeNode("New Node");
+            treeView1.Nodes.Add(newNode);
+        }
+
+        // Remove Node button click event handler
+        private void RemoveNodeButton_Click(object sender, EventArgs e)
+        {
+            foreach (TreeNode node in treeView1.Nodes)
+            {
+                RemoveCheckedNodes(node);
+            }
+        }
+
+        // Remove checked nodes recursively
+        private void RemoveCheckedNodes(TreeNode parentNode)
+        {
+            for (int i = parentNode.Nodes.Count - 1; i >= 0; i--)
+            {
+                TreeNode node = parentNode.Nodes[i];
+                if (node.Checked)
+                {
+                    parentNode.Nodes.Remove(node);
+                }
+                else
+                {
+                    RemoveCheckedNodes(node);
+                }
+            }
+        }
+
+        // AfterCheck event handler
+        private void TreeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            // Your code here if you want to perform any action after checking a node
+        }
+    }
+}
